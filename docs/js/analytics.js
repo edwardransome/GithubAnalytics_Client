f62abf75d1f1e72e28c1aa1d073f0ad9dbbdf625
Edward Ransome <edward.ransome@heig-vd.ch>
2017-10-21 18:56:09 +0200

    added json folder in non restricted area

--- docs/js/analytics.js
+++ docs/js/analytics.js
@@ -17,22 +17,44 @@ readTextFile("./test.json", function(text){
     console.log(data);
 });
 
-var ctx1_data;
-
+var ctx1_data = [12,4,76,2,43,90];
+/*
 for(var i = 0; i < data.length; i++){
-    console.log(data[i]);
+    a = data[3] - data[2];
+    if(a < day){
+        ctx1_data[0]++; 
+    }else if (a < day){
+        ctx1_data[1]++; 
+    }else if (a < day){
+        ctx1_data[2]++; 
+    }else if (a < day){
+        ctx1_data[3]++; 
+    }else if (a < day){
+        ctx1_data[4]++; 
+    }else{
+        ctx1_data[5]++;
+    }
 }
-
+*/
 var ctx1 = document.getElementById("requestsOverTime").getContext('2d');
 var ctx2 = document.getElementById("timeBeforeClosed").getContext('2d');
 var ctx3 = document.getElementById("requestsOpenTooLong").getContext('2d');
+
 var timeBeforeClosed = new Chart(ctx2, {
     type: 'bar',
     data: {
         labels: ["Under 24h", "1-2 Days", "3-4 Days", "5-6 Days", "1 - 2 Weeks", "More than 2 Weeks"],
         datasets: [{
             label: 'Time before request was closed',
-            data: [12, 19, 3, 5, 2, 3],
+            data: ctx1_data,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
         }]
     },
     options: {
