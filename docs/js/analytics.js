b878db9853b8ff6c2f436af339f6ba99186548f9
Edward Ransome <edward.ransome@heig-vd.ch>
2017-10-21 23:04:02 +0200

    Synchronous load json -> back to async

--- docs/js/analytics.js
+++ docs/js/analytics.js
@@ -2,7 +2,7 @@
 function readTextFile(file, callback) {
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, false);
+    rawFile.open("GET", file, true);
     
     rawFile.onreadystatechange = function() {           
         if (rawFile.readyState === 4 && rawFile.status == "200") {            
